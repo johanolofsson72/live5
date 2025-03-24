3c82a6a5c53f6cec4aedab1348cb0ab76095986d
Johan Olofsson <jool@me.com>
2025-03-24 19:41:37 +0100

    Add reminder to remove deployment after resource group deletion in remove-env.ps1

--- env/remove-env.ps1
+++ env/remove-env.ps1
@@ -5,3 +5,5 @@ Write-Host "🗑️ Tar bort hela resursgruppen '$resourceGroup'..."
 az group delete --name $resourceGroup --yes --no-wait
 
 Write-Host "⏳ Borttagning initierad. Det kan ta några minuter."
+
+Write-Host " Gläm inte köra 'az deployment group delete --resource-group live5rg --name env' efteråt för att ta bort deploymenten."
